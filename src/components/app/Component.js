3ede9e3028c89fb3ada69378477a8fd4099cf9e0
Federico Bertolini <fb@hellofresh.com>
2017-01-01 19:58:16 +0100

    sass loader config

--- src/components/app/Component.js
+++ src/components/app/Component.js
@@ -4,9 +4,10 @@ import HomeContainer from '../home/Container';
 import GameCreationContainer from '../gameCreation/Container';
 import RoundTableContainer from '../roundTable/Container';
 import RoundCreationContainer from '../roundCreation/Container';
+require('./style.scss');
 
 const App = () => (
-    <div>
+    <div className="page-container">
         <h1>UNO Score Tracker</h1>
 
         <Router history={ hashHistory }>

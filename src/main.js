91b18e8fe517e0631c20d7a5b60c726e35a1602a
Federico Bertolini <fb@hellofresh.com>
2017-01-19 19:14:13 +0100

    reduce bundle size

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/app/Component';
@@ -24,7 +24,7 @@ if (!store.getState().game.status) {
     store.dispatch(createGame());
 }
 
-ReactDOM.render(
+render(
     <Provider store={store}>
       <App />
     </Provider>,
